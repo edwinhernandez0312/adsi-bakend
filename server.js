308f1c67c2cc0b2172284e56b6b9c41cc8ef2a95
edwinhernandez0312 <eh520856@gmail.com>
2021-03-23 15:42:11 -0500

    arreglo

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ import dbconecction from './satabase/config.js';
 
 class server{
     constructor(){
-        // 1 crear servidor
+       
         this.app = express();
-        this.port=process.env.port;
-        // 2 conectarnos a la base de datos
+        this.PORT=process.env.PORT;
+       
         this.conectarBD();
-        // 3 darle a conocer todos los middleware
+     
         this.middleware();
-        // 4 rutas o routers que tenemos
+        
         this.routes()
     }
 
@@ -27,13 +27,13 @@ class server{
 middleware(){
     this.app.use(express.json());
     this.app.use(cors());
-    // ya viene integrado con express para lo de las rutas en static es donde se busca de donde estara el form
+   
     this.app.use(express.static('public'))
 }
 
 listen(){
     this.app.listen(this.port,()=>{
-        console.log(`servidor corriendo en el puerto ${this.port}`);
+        console.log(`servidor corriendo en el puerto ${this.PORT}`);
     
     })
 }
